ccb1a514eb1829b651b20b290fb41fae678b74c4
lbroudoux <laurent.broudoux@gmail.com>
2019-08-14 16:13:01 +0200

    #8 Adding container image with microcks-cli

--- build-binaries.sh
+++ build-binaries.sh
@@ -20,7 +20,7 @@ do
         output_name+='.exe'
     fi  
 
-    env GOOS=$GOOS GOARCH=$GOARCH go build -o $output_name $package
+    env GOOS=$GOOS GOARCH=$GOARCH go build -o ./build/_output/$output_name $package
     if [ $? -ne 0 ]; then
         echo 'An error has occurred! Aborting the script execution...'
         exit 1
