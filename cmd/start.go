37fce3447a0bbfe3014c1fb32b285840db11229e
Luxxgit2k4 <lakshmanoni1234@gmail.com>
2025-02-22 03:17:01 +0530

    Viper is added for port configuration while starting the server
    Signed-off-by: Luxxgit2k4 <lakshmanoni1234@gmail.com>

--- cmd/start.go
+++ cmd/start.go
@@ -6,15 +6,18 @@ import (
 	"os/exec"
 
 	"github.com/spf13/cobra"
+  "github.com/spf13/viper"
 )
 
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "Start the Microcks server",
+	Short: "Start the Microcks server with your desired port or default port",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Starting Microcks server...")
+    // Set the port for the server
+    port := viper.GetInt("port")
+		fmt.Printf("Starting Microcks server on port %d...\n", port)
 
-		startCmd := exec.Command("docker", "run", "-d", "-p", "8585:8080", "--rm", "--name", "microcks-server", "quay.io/microcks/microcks-uber:latest-native")
+		startCmd := exec.Command("docker", "run", "-d", "-p", fmt.Sprintf("%d:8080", port), "--rm", "--name", "microcks-server", "quay.io/microcks/microcks-uber:latest-native")
 
 		startCmd.Stdout = nil
 		startCmd.Stderr = os.Stderr
@@ -23,9 +26,15 @@ var startCmd = &cobra.Command{
 			fmt.Printf("Error starting Microcks: %v\n", err)
 			os.Exit(1)
 		}
+    fmt.Printf("Microcks server is running on localhost:%d\n", port)
 	},
 }
 
 func init() {
+	// Add the start command to the root command
 	rootCmd.AddCommand(startCmd)
+
+	// Set up Viper to read from flags
+	startCmd.Flags().Int("port", 8585, "Port for Microcks server")
+	viper.BindPFlag("port", startCmd.Flags().Lookup("port"))
 }
