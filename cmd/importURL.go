dd1eab3940bf51d4e906d909de4f14d93cf94674
Harshvardhan Parmar <harshparmar4902@gmail.com>
2025-03-13 21:31:39 +0530

    fixes #124 feat: adapt spf13/cobra library (#125)
    Signed-off-by: Harsh4902 <harshparmar4902@gmail.com>

--- cmd/importURL.go
+++ cmd/importURL.go
@@ -1,7 +1,22 @@
+/*
+ * Copyright The Microcks Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package cmd
 
 import (
-	"flag"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,120 +24,132 @@ import (
 
 	"github.com/microcks/microcks-cli/pkg/config"
 	"github.com/microcks/microcks-cli/pkg/connectors"
+	"github.com/spf13/cobra"
 )
 
-type importURLCommand struct {
-}
+// import (
+// 	"flag"
+// 	"fmt"
+// 	"os"
+// 	"strconv"
+// 	"strings"
 
-func NewImportURLCommand() Command {
-	return new(importURLCommand)
-}
+// 	"github.com/microcks/microcks-cli/pkg/config"
+// 	"github.com/microcks/microcks-cli/pkg/connectors"
+// )
 
-func (c *importURLCommand) Execute() {
-	// Parse subcommand args first.
-	if len(os.Args) < 2 {
-		fmt.Println("import-url command require <specificationFile1URL[:primary],specificationFile2URL[:primary]> args")
-		os.Exit(1)
-	}
+// type importURLCommand struct {
+// }
 
-	specificationFiles := os.Args[2]
+func NewImportURLCommand() *cobra.Command {
+	var (
+		microcksURL          string
+		keycloakClientID     string
+		keycloakClientSecret string
+		insecureTLS          bool
+		caCertPaths          string
+		verbose              bool
+	)
+	var importURLCmd = &cobra.Command{
+		Use:   "import-url",
+		Short: "import API artifacts from URL on Microcks server",
+		Long:  `import API artifacts from URL on Microcks server`,
+		Run: func(cmd *cobra.Command, args []string) {
+			// Parse subcommand args first.
+			if len(os.Args) < 2 {
+				fmt.Println("import-url command require <specificationFile1URL[:primary],specificationFile2URL[:primary]> args")
+				os.Exit(1)
+			}
 
-	// Then parse flags.
-	importCmd := flag.NewFlagSet("import-url", flag.ExitOnError)
+			specificationFiles := os.Args[2]
 
-	var microcksURL string
-	var keycloakURL string
-	var keycloakClientID string
-	var keycloakClientSecret string
-	var insecureTLS bool
-	var caCertPaths string
-	var verbose bool
+			// Validate presence and values of flags.
+			if len(microcksURL) == 0 {
+				fmt.Println("--microcksURL flag is mandatory. Check Usage.")
+				os.Exit(1)
+			}
+			if len(keycloakClientID) == 0 {
+				fmt.Println("--keycloakClientId flag is mandatory. Check Usage.")
+				os.Exit(1)
+			}
+			if len(keycloakClientSecret) == 0 {
+				fmt.Println("--keycloakClientSecret flag is mandatory. Check Usage.")
+				os.Exit(1)
+			}
 
-	importCmd.StringVar(&microcksURL, "microcksURL", "", "Microcks API URL")
-	importCmd.StringVar(&keycloakClientID, "keycloakClientId", "", "Keycloak Realm Service Account ClientId")
-	importCmd.StringVar(&keycloakClientSecret, "keycloakClientSecret", "", "Keycloak Realm Service Account ClientSecret")
-	importCmd.BoolVar(&insecureTLS, "insecure", false, "Whether to accept insecure HTTPS connection")
-	importCmd.StringVar(&caCertPaths, "caCerts", "", "Comma separated paths of CRT files to add to Root CAs")
-	importCmd.BoolVar(&verbose, "verbose", false, "Produce dumps of HTTP exchanges")
-	importCmd.Parse(os.Args[3:])
+			// Collect optional HTTPS transport flags.
+			if insecureTLS {
+				config.InsecureTLS = true
+			}
+			if len(caCertPaths) > 0 {
+				config.CaCertPaths = caCertPaths
+			}
+			if verbose {
+				config.Verbose = true
+			}
 
-	// Validate presence and values of flags.
-	if len(microcksURL) == 0 {
-		fmt.Println("--microcksURL flag is mandatory. Check Usage.")
-		os.Exit(1)
-	}
-	if len(keycloakClientID) == 0 {
-		fmt.Println("--keycloakClientId flag is mandatory. Check Usage.")
-		os.Exit(1)
-	}
-	if len(keycloakClientSecret) == 0 {
-		fmt.Println("--keycloakClientSecret flag is mandatory. Check Usage.")
-		os.Exit(1)
-	}
+			// Now we seems to be good ...
+			// First - retrieve the Keycloak URL from Microcks configuration.
+			mc := connectors.NewMicrocksClient(microcksURL)
+			keycloakURL, err := mc.GetKeycloakURL()
+			if err != nil {
+				fmt.Printf("Got error when invoking Microcks client retrieving config: %s", err)
+				os.Exit(1)
+			}
 
-	// Collect optional HTTPS transport flags.
-	if insecureTLS {
-		config.InsecureTLS = true
-	}
-	if len(caCertPaths) > 0 {
-		config.CaCertPaths = caCertPaths
-	}
-	if verbose {
-		config.Verbose = true
-	}
+			var oauthToken string = "unauthentifed-token"
+			if keycloakURL != "null" {
+				//  If Keycloak is enabled, retrieve an OAuth token using Keycloak Client.
+				kc := connectors.NewKeycloakClient(keycloakURL, keycloakClientID, keycloakClientSecret)
 
-	// Now we seems to be good ...
-	// First - retrieve the Keycloak URL from Microcks configuration.
-	mc := connectors.NewMicrocksClient(microcksURL)
-	keycloakURL, err := mc.GetKeycloakURL()
-	if err != nil {
-		fmt.Printf("Got error when invoking Microcks client retrieving config: %s", err)
-		os.Exit(1)
-	}
-
-	var oauthToken string = "unauthentifed-token"
-	if keycloakURL != "null" {
-		//  If Keycloak is enabled, retrieve an OAuth token using Keycloak Client.
-		kc := connectors.NewKeycloakClient(keycloakURL, keycloakClientID, keycloakClientSecret)
-
-		oauthToken, err = kc.ConnectAndGetToken()
-		if err != nil {
-			fmt.Printf("Got error when invoking Keycloack client: %s", err)
-			os.Exit(1)
-		}
-	}
-
-	// Then - for each specificationFile, upload the artifact on Microcks Server.
-	mc.SetOAuthToken(oauthToken)
-
-	sepSpecificationFiles := strings.Split(specificationFiles, ",")
-	for _, f := range sepSpecificationFiles {
-		mainArtifact := true
-		secret := ""
-
-		// Check if URL starts with https or http
-		if strings.HasPrefix(f, "https://") || strings.HasPrefix(f, "http://") {
-			urlAndMainAtrifactAndSecretName := strings.Split(f, ":")
-			n := len(urlAndMainAtrifactAndSecretName)
-			f = urlAndMainAtrifactAndSecretName[0] + ":" + urlAndMainAtrifactAndSecretName[1]
-			if n > 2 {
-				val, err := strconv.ParseBool(urlAndMainAtrifactAndSecretName[2])
+				oauthToken, err = kc.ConnectAndGetToken()
 				if err != nil {
-					fmt.Println(err)
+					fmt.Printf("Got error when invoking Keycloack client: %s", err)
+					os.Exit(1)
 				}
-				mainArtifact = val
 			}
-			if n > 3 {
-				secret = urlAndMainAtrifactAndSecretName[3]
-			}
-		}
 
-		// Try downloading the artifcat
-		msg, err := mc.DownloadArtifact(f, mainArtifact, secret)
-		if err != nil {
-			fmt.Printf("Got error when invoking Microcks client importing Artifact: %s", err)
-			os.Exit(1)
-		}
-		fmt.Printf("Microcks has discovered '%s'\n", msg)
+			// Then - for each specificationFile, upload the artifact on Microcks Server.
+			mc.SetOAuthToken(oauthToken)
+
+			sepSpecificationFiles := strings.Split(specificationFiles, ",")
+			for _, f := range sepSpecificationFiles {
+				mainArtifact := true
+				secret := ""
+
+				// Check if URL starts with https or http
+				if strings.HasPrefix(f, "https://") || strings.HasPrefix(f, "http://") {
+					urlAndMainAtrifactAndSecretName := strings.Split(f, ":")
+					n := len(urlAndMainAtrifactAndSecretName)
+					f = urlAndMainAtrifactAndSecretName[0] + ":" + urlAndMainAtrifactAndSecretName[1]
+					if n > 2 {
+						val, err := strconv.ParseBool(urlAndMainAtrifactAndSecretName[2])
+						if err != nil {
+							fmt.Println(err)
+						}
+						mainArtifact = val
+					}
+					if n > 3 {
+						secret = urlAndMainAtrifactAndSecretName[3]
+					}
+				}
+
+				// Try downloading the artifcat
+				msg, err := mc.DownloadArtifact(f, mainArtifact, secret)
+				if err != nil {
+					fmt.Printf("Got error when invoking Microcks client importing Artifact: %s", err)
+					os.Exit(1)
+				}
+				fmt.Printf("Microcks has discovered '%s'\n", msg)
+			}
+		},
 	}
+	importURLCmd.Flags().StringVar(&microcksURL, "microcksURL", "", "Microcks API URL")
+	importURLCmd.Flags().StringVar(&keycloakClientID, "keycloakClientId", "", "Keycloak Realm Service Account ClientId")
+	importURLCmd.Flags().StringVar(&keycloakClientSecret, "keycloakClientSecret", "", "Keycloak Realm Service Account ClientSecret")
+	importURLCmd.Flags().BoolVar(&insecureTLS, "insecure", false, "Whether to accept insecure HTTPS connection")
+	importURLCmd.Flags().StringVar(&caCertPaths, "caCerts", "", "Comma separated paths of CRT files to add to Root CAs")
+	importURLCmd.Flags().BoolVar(&verbose, "verbose", false, "Produce dumps of HTTP exchanges")
+
+	return importURLCmd
 }
--- main.go
+++ main.go
@@ -1,35 +1,9 @@
 package main
 
 import (
-	"os"
-
 	"github.com/microcks/microcks-cli/cmd"
 )
 
 func main() {
-	var c cmd.Command
-
-	if len(os.Args) == 1 {
-		cmd.NewHelpCommand().Execute()
-		os.Exit(1)
-	}
-
-	switch os.Args[1] {
-	case "version":
-		c = cmd.NewVersionCommand()
-	case "help":
-		c = cmd.NewHelpCommand()
-	case "test":
-		c = cmd.NewTestCommand()
-	case "import":
-		c = cmd.NewImportCommand()
-	case "import-url":
-		c = cmd.NewImportURLCommand()
-	default:
-		cmd.NewHelpCommand().Execute()
-		os.Exit(1)
-	}
-
-	c.Execute()
-	return
+	cmd.Execute()
 }
