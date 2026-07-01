6e78f5296522a2094c4a95896f14a0ade4214e4e
caesarsage <destinyerhabor6@gmail.com>
2026-07-01 09:49:26 +0100

    feat(test): thread --artifact to formatters for diff annotations
    Signed-off-by: caesarsage <destinyerhabor6@gmail.com>

--- cmd/test.go
+++ cmd/test.go
@@ -119,6 +119,7 @@ func NewTestCommand(globalClientOpts *connectors.ClientOptions) *cobra.Command {
 				operationsHeaders:  operationsHeaders,
 				oAuth2Context:      oAuth2Context,
 				outputFormat:       outputFormat,
+				artifactPath:       artifact,
 			}
 
 			if !dryRun {
