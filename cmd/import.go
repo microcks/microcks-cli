f5a63b557350d668cb0cb83d83463d3ea54927dd
ish-g09 <ig.valiente09@gmail.com>
2026-09-18 16:16:48 +0530

    refactor: use filepath.Clean for robust path normalization
    Signed-off-by: ish-g09 <ig.valiente09@gmail.com>

--- cmd/import.go
+++ cmd/import.go
@@ -18,6 +18,7 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"strconv"
 	"strings"
 
@@ -173,7 +174,7 @@ func NewImportCommand(globalClientOpts *connectors.ClientOptions) *cobra.Command
 					}
 
 					// Normalize file path to match the watcher fsnotify events format.
-					f = strings.TrimPrefix(f, "./")
+					f = filepath.Clean(f)
 
 					// Upsert entry.
 					watchCfg.UpsertEntry(config.WatchEntry{
